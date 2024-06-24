6915e97d29114a95bf3840afdf3272e32a040a59
Oliver Morland <oliversimonedward@hotmail.co.uk>
2024-06-24 16:11:44 -0400

    New hierachy structure

---
+++ Assets/Scripts/AICharacter.cs
@@ -0,0 +1,133 @@
+using UnityEngine;
+using UnityEngine.Events;
+using Oculus.Voice.Dictation;
+using TMPro;
+using Meta.WitAi.TTS.Utilities;
+using OpenAIForUnity;
+
+public class AICharacter : MonoBehaviour
+{
+    public OpenAIAssistant aiAssistant; 
+    public UnityEvent<string> onResponseEvent = new UnityEvent<string>();
+    public AppDictationExperience voiceToText;
+    public TMP_Text voiceInputLabel;
+    public OpenAIConfigurationSO openAIConfiguration;
+    public TTSSpeaker textToSpeechSpeaker;
+    string currentOpenAIResponse;
+
+    [Header("Physical Interaction Settings")]
+    public float minConversationRange = 5f;
+    Transform userView;
+    public Transform avatarTransform;
+    public TMP_Text npcFeedbackText;
+    public enum NPCState { None, IsListening, IsThinking, IsTalking};
+    public NPCState currentNPCState = NPCState.None;
+    public Animator animator;
+    const string LISTEN_TRIGGER = "Listen";
+    const string THINK_TRIGGER = "Think";
+    const string TALK_TRIGGER = "Talk";
+    const string IDLE_TRIGGER = "Idle";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        voiceToText.DictationEvents.OnPartialTranscription.AddListener(OnPartialDescription);
+        voiceToText.DictationEvents.OnFullTranscription.AddListener(OnFullTranscription);
+        textToSpeechSpeaker.Events.OnAudioClipPlaybackStart.AddListener(OnTextToSpeechStarted);
+        textToSpeechSpeaker.Events.OnAudioClipPlaybackFinished.AddListener(OnTextToSpeechFinished);
+        aiAssistant.OnResponseRecieved.AddListener(OnAIAssistantResponse);
+        onResponseEvent.AddListener(OnResponse);
+        userView = Camera.main.transform;
+        voiceInputLabel.text = "";
+    }
+
+    private void OnAIAssistantResponse(string response)
+    {
+        onResponseEvent.Invoke(response);
+    }
+
+    private void OnPartialDescription(string transcript)
+    {
+        if (currentNPCState == NPCState.IsListening)
+        {
+            voiceInputLabel.text = transcript;
+        }
+    }
+
+    void OnFullTranscription(string transcription)
+    {
+        if (currentNPCState == NPCState.IsListening)
+        {
+            voiceInputLabel.text = transcription;
+            aiAssistant.AskAssistant(transcription);
+            SetNPCState(NPCState.IsThinking);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 userToHeadVector = avatarTransform.position - userView.transform.position;
+        float distanceSquared = userToHeadVector.sqrMagnitude;
+        if (distanceSquared < minConversationRange)
+        {
+            if (currentNPCState == NPCState.None)
+            {
+                SetNPCState(NPCState.IsListening);
+            }
+        }
+        else
+        {
+            if (currentNPCState == NPCState.IsListening)
+            {
+                SetNPCState(NPCState.None);
+            }
+        }
+    }
+
+    void SetNPCState(NPCState desiredNPCState)
+    {
+        currentNPCState = desiredNPCState;
+
+        switch (currentNPCState)
+        {
+            case NPCState.None:
+                //Do nothing
+                npcFeedbackText.text = "";
+                animator.SetTrigger(IDLE_TRIGGER);
+                voiceToText.Deactivate();
+                break;
+            case NPCState.IsListening:
+                npcFeedbackText.text = "Listening...";
+                animator.SetTrigger(LISTEN_TRIGGER);
+                voiceToText.Activate();
+                break;
+            case NPCState.IsThinking:
+                npcFeedbackText.text = "Thinking...";
+                animator.SetTrigger(THINK_TRIGGER);
+                voiceToText.Deactivate();
+                break;
+            case NPCState.IsTalking:
+                animator.SetTrigger(TALK_TRIGGER);
+                npcFeedbackText.text = currentOpenAIResponse;
+                voiceInputLabel.text = "";
+                break;
+        }
+    }
+
+    void OnResponse(string message)
+    {
+        currentOpenAIResponse = message;
+    }
+
+    void OnTextToSpeechStarted(AudioClip clip)
+    {
+        SetNPCState(NPCState.IsTalking);
+    }
+
+    private void OnTextToSpeechFinished(AudioClip clip)
+    {
+        SetNPCState(NPCState.None);
+    }
+}
---
+++ Assets/Scripts/OpenAIAssistant.cs
@@ -0,0 +1,301 @@
+using System;
+using System.Collections;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Networking;
+
+namespace OpenAIForUnity
+{
+    public class OpenAIAssistant : MonoBehaviour
+    {
+        public OpenAIConfigurationSO openAIConfig;
+        public string assistantId = "asst_iQOTrPcuusPL2UEvd5gNjTHl";
+        public UnityEvent<string> OnResponseRecieved;
+        private string threadId;
+        const string apiEndPointRoot = "https://api.openai.com/v1/threads";
+        const float LIST_MESSAGES_INTERVAL = 0.5f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            CreateNewThread();
+        }
+
+        void CreateNewThread()
+        {
+            WebRequestData requestData = new WebRequestData();
+            requestData.path = apiEndPointRoot;
+            requestData.methodType = WebRequestData.MethodType.POST;
+            DispatchWebRequest(requestData,
+            failedResult =>
+            {
+                Debug.LogError(failedResult);
+            },
+            successResult =>
+            {
+                CreateThreadResult createThreadResponse = JsonUtility.FromJson<CreateThreadResult>(successResult);
+                threadId = createThreadResponse.id;
+            });
+        }
+
+        void DispatchWebRequest(WebRequestData webRequestData, Action<string> onRequestFailed, Action<string> onRequestSucceeded)
+        {
+            UnityWebRequest webRequest = CreateWebRequest(webRequestData);
+            StartCoroutine(DispatchWebRequestAsync(webRequest, onRequestFailed, onRequestSucceeded));
+        }
+
+        UnityWebRequest CreateWebRequest(WebRequestData webRequestData)
+        {
+            string methodAsString = GetMethodAsString(webRequestData.methodType);
+            UnityWebRequest webRequest = new UnityWebRequest(webRequestData.path, methodAsString);
+            webRequest.uploadHandler = new UploadHandlerRaw(webRequestData.body);
+            webRequest.downloadHandler = new DownloadHandlerBuffer();
+            webRequest.SetRequestHeader("Content-Type", "application/json");
+            webRequest.SetRequestHeader("OpenAI-Beta", "assistants=v2");
+            webRequest.SetRequestHeader("Authorization", "Bearer " + openAIConfig.secretAPIKey);
+            return webRequest;
+        }
+
+        string GetMethodAsString(WebRequestData.MethodType methodType)
+        {
+            string methodAsString = "";
+            switch (methodType)
+            {
+                case WebRequestData.MethodType.POST:
+                    methodAsString = "POST";
+                    break;
+                case WebRequestData.MethodType.GET:
+                    methodAsString = "GET";
+                    break;
+                default:
+                    methodAsString = "GET";
+                    break;
+            }
+            return methodAsString;
+        }
+
+        IEnumerator DispatchWebRequestAsync(UnityWebRequest webRequest, Action<string> onRequestFailed, Action<string> onRequestSucceeded)
+        {
+            yield return webRequest.SendWebRequest();
+            if (RequestHasFailed(webRequest))
+            {
+                onRequestFailed.Invoke(webRequest.error);
+            }
+            else
+            {
+                onRequestSucceeded.Invoke(webRequest.downloadHandler.text);
+            }
+        }
+
+        public void AskAssistant(string userMessage)
+        {
+            AddMessage(userMessage);
+        }
+
+        void AddMessage(string userMessage)
+        {
+            DispatchAddMessageRequest(userMessage,
+            failedResult =>
+            {
+                Debug.LogError(failedResult);
+            },
+            succeededResult =>
+            {
+                Debug.Log(succeededResult);
+                RunMessages();
+            });
+        }
+
+        void RunMessages()
+        {
+            DispatchRunRequest(
+            failedResult =>
+            {
+                Debug.LogError(failedResult);
+            },
+            succeededResult =>
+            {
+                Debug.Log(succeededResult);
+                RunResult runResult = JsonUtility.FromJson<RunResult>(succeededResult);
+                string runId = runResult.id;
+                StartCoroutine(WaitForAssistantResponse(runId));
+            });
+        }
+
+        IEnumerator WaitForAssistantResponse(string runId)
+        {
+            Message[] messages = new Message[0];
+            int requestAttempts = 0;
+            while (MessagesHaveContents(messages) == false)
+            {
+                yield return new WaitForSeconds(LIST_MESSAGES_INTERVAL);
+                UnityWebRequest getMessagesRequest = CreateListMessagesRequest(runId);
+                yield return getMessagesRequest.SendWebRequest();
+                requestAttempts++;
+                if (RequestHasFailed(getMessagesRequest))
+                {
+                    Debug.LogError(getMessagesRequest.error);
+                    yield break;
+                }
+                ListMessagesResult getMessagesResponse = JsonUtility.FromJson<ListMessagesResult>(getMessagesRequest.downloadHandler.text);
+                messages = getMessagesResponse.data;
+            }
+            Debug.Log($"Message found after {requestAttempts} attempts");
+            string response = GetResponseFromMessages(messages);
+            Debug.Log(response);
+            OnResponseRecieved.Invoke(response);
+        }
+
+        bool MessagesHaveContents(Message[] messages)
+        {
+            if (messages.Length > 0)
+            {
+                foreach (Message message in messages)
+                {
+                    if (message.content != null)
+                    {
+                        if (message.content.Length > 0)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        UnityWebRequest CreateListMessagesRequest(string runId)
+        {
+            WebRequestData requestData = new WebRequestData();
+            requestData.path = $"{apiEndPointRoot}/{threadId}/messages?run_id={runId}";
+            requestData.methodType = WebRequestData.MethodType.GET;
+            return CreateWebRequest(requestData);
+        }
+
+        bool RequestHasFailed(UnityWebRequest unityWebRequest)
+        {
+            if (unityWebRequest.result == UnityWebRequest.Result.ConnectionError || unityWebRequest.result == UnityWebRequest.Result.ProtocolError)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        string GetResponseFromMessages(Message[] messages)
+        {
+            string response = "";
+            Message firstMessage = messages[0];
+            MessageContent[] messageContents = firstMessage.content;
+            Debug.Log("Message Contents length: " + messageContents.Length);
+            foreach (var content in messageContents)
+            {
+                response = content.text.value;
+            }
+            return response;
+        }
+
+        void DispatchAddMessageRequest(string userMessage, Action<string> onRequestFailed, Action<string> onRequestSucceeded)
+        {
+            WebRequestData requestData = new WebRequestData();
+            requestData.path = $"{apiEndPointRoot}/{threadId}/messages";
+            requestData.methodType = WebRequestData.MethodType.POST;
+            requestData.body = CreateAddMessageRequestBody(userMessage);
+            DispatchWebRequest(requestData, onRequestFailed, onRequestSucceeded);
+        }
+
+        byte[] CreateAddMessageRequestBody(string userMessage)
+        {
+            AddMessageRequestBody body = new AddMessageRequestBody();
+            body.role = "user";
+            body.content = userMessage;
+            string bodyJson = JsonUtility.ToJson(body);
+            return Encoding.UTF8.GetBytes(bodyJson);
+        }
+
+        void DispatchRunRequest(Action<string> onRequestFailed, Action<string> onRequestSucceeded)
+        {
+            WebRequestData requestData = new WebRequestData();
+            requestData.path = $"{apiEndPointRoot}/{threadId}/runs";
+            requestData.methodType = WebRequestData.MethodType.POST;
+            requestData.body = CreateRunRequestBody(assistantId);
+            DispatchWebRequest(requestData, onRequestFailed, onRequestSucceeded);
+        }
+
+        byte[] CreateRunRequestBody(string assistantId)
+        {
+            RunRequestBody body = new RunRequestBody();
+            body.assistant_id = assistantId;
+            string bodyJson = JsonUtility.ToJson(body);
+            return Encoding.UTF8.GetBytes(bodyJson);
+        }
+
+        public struct WebRequestData
+        {
+            public string path;
+            public enum MethodType { POST, GET }
+            public MethodType methodType;
+            public byte[] body;
+        }
+
+        [System.Serializable]
+        public struct CreateThreadResult
+        {
+            public string id;
+        }
+
+        [System.Serializable]
+        public struct AddMessageRequestBody
+        {
+            public string role;
+            public string content;
+        }
+
+        [System.Serializable]
+        public struct RunRequestBody
+        {
+            public string assistant_id;
+        }
+
+        [System.Serializable]
+        public struct RunResult
+        {
+            public string id;
+            public string last_error;
+            public string instructions;
+            public string model;
+        }
+
+        [System.Serializable]
+        public struct ListMessagesResult
+        {
+            public Message[] data;
+        }
+
+        [System.Serializable]
+        public struct Message
+        {
+            public string id;
+            public string role;
+            public MessageContent[] content;
+        }
+
+        [System.Serializable]
+        public struct MessageContent
+        {
+            public string type;
+            public MessageText text;
+        }
+
+        [System.Serializable]
+        public struct MessageText
+        {
+            public string value;
+            public string[] annotations;
+        }
+
+    }
+}
